96690415fae3d21fa89d529cbe22015c80a9d1a2
michaelcw02 <michael.cw02@hotmail.com>
2016-10-28 06:52:28 -0600

    basics relations between 'Inventario' and 'Producto'

--- ModeloSistemaFerreteria/src/modelo/inventarios/Inventario.java
+++ ModeloSistemaFerreteria/src/modelo/inventarios/Inventario.java
@@ -5,10 +5,70 @@
  */
 package modelo.inventarios;
 
+import java.util.Objects;
+import modelo.productos.Producto;
+import org.joda.time.DateTime;
+
+
 /**
  *
  * @author Michael Chen W.
  */
 public class Inventario {
+
+    public Inventario(DateTime fecha, Producto producto, int cantidad) {
+        this.fecha = fecha;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        if(cantidad > 0)
+            activo = true;
+    }
+
+    public void setFecha(DateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public DateTime getFecha() {
+        return fecha;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Inventario other = (Inventario) obj;
+        if (!Objects.equals(this.fecha, other.fecha)) {
+            return false;
+        }
+        if (!Objects.equals(this.producto, other.producto)) {
+            return false;
+        }
+        return true;
+    }
     
+    DateTime fecha;
+    Producto producto;
+    int cantidad;
+    boolean activo = false;
 }
---
+++ ModeloSistemaFerreteria/src/modelo/inventarios/ListaInventarios.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.inventarios;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Michael Chen W.
+ */
+public class ListaInventarios {
+
+    public ListaInventarios() {
+        inventarios = new LinkedList<>();
+    }
+    
+    public void agregar(Inventario item) {
+        inventarios.add(item);
+    }
+    
+    LinkedList<Inventario> inventarios;
+}
