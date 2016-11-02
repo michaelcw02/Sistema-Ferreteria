8a01b2ebd731e68ccbda7b5bbac179a14d6e1a04
Laura Alfaro <Laura Alfaro@192.168.1.5>
2016-11-02 14:49:44 -0600

    insert empleado window

--- SistemaFerreteria/src/Interfaz/VMantListaEmple.java
+++ SistemaFerreteria/src/Interfaz/VMantListaEmple.java
@@ -6,6 +6,8 @@
 package Interfaz;
 
 import control.Control;
+import java.awt.Color;
+import javax.swing.ButtonGroup;
 
 /**
  *
@@ -18,7 +20,8 @@ public class VMantListaEmple extends javax.swing.JFrame {
      */
     public VMantListaEmple(Control c) {
         initComponents();
-        ctrl=c;
+        ctrl = c;
+       
     }
 
     /**
@@ -30,121 +33,282 @@ public class VMantListaEmple extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        panel = new javax.swing.JPanel();
-        Busqueda = new javax.swing.JButton();
-        Inclusion = new javax.swing.JButton();
-        Modificacion = new javax.swing.JButton();
-        Consulta = new javax.swing.JButton();
-        Borrado = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        jTabbedPane2 = new javax.swing.JTabbedPane();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        txtFieldNombre = new javax.swing.JTextField();
+        txtFieldCedula = new javax.swing.JTextField();
+        txtFieldClave = new javax.swing.JTextField();
+        btnAgregar = new javax.swing.JButton();
+        lblExistencia = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        vendedor = new javax.swing.JRadioButton();
+        cajero = new javax.swing.JRadioButton();
+        btnTrue1 = new javax.swing.JRadioButton();
+        btnTrue2 = new javax.swing.JRadioButton();
+        btnTrue3 = new javax.swing.JRadioButton();
+        jPanel3 = new javax.swing.JPanel();
+        jPanel4 = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout panelLayout = new javax.swing.GroupLayout(panel);
-        panel.setLayout(panelLayout);
-        panelLayout.setHorizontalGroup(
-            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
-        );
-        panelLayout.setVerticalGroup(
-            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 172, Short.MAX_VALUE)
-        );
-
-        Busqueda.setText("Búsqueda");
-
-        Inclusion.setText("Inclusión");
-        Inclusion.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                InclusionActionPerformed(evt);
-            }
-        });
-
-        Modificacion.setText("Modificación");
-        Modificacion.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                ModificacionActionPerformed(evt);
-            }
-        });
-
-        Consulta.setText("Consulta");
-        Consulta.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                ConsultaActionPerformed(evt);
-            }
-        });
-
-        Borrado.setText("Borrado");
-
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jLabel1.setText("MANTENIMIENTO EMPLEADOS");
+
+        jLabel2.setText("Nombre del empleado: ");
+
+        jLabel3.setText("Cedula del empleado:");
+
+        jLabel4.setText("Clave de empleado:");
+
+        txtFieldNombre.setText("<Nombre(String)>");
+        txtFieldNombre.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txtFieldNombreFocusLost(evt);
+            }
+        });
+
+        txtFieldCedula.setText("<Cedula(String)>");
+
+        txtFieldClave.setText("<Clave (String)>");
+        txtFieldClave.setPreferredSize(new java.awt.Dimension(160, 24));
+
+        btnAgregar.setText("Agregar");
+        btnAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgregarActionPerformed(evt);
+            }
+        });
+
+        lblExistencia.setText("<Existencia de empleado>");
+
+        jLabel6.setText("Roles:");
+
+        vendedor.setText("Vendedor");
+
+        cajero.setText("Cajero");
+
+        btnTrue1.setText("Despachador");
+        btnTrue1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnTrue1ActionPerformed(evt);
+            }
+        });
+
+        btnTrue2.setText("Administrador");
+
+        btnTrue3.setText("Bodeguero");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel6)
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(vendedor)
+                                    .addComponent(btnTrue3))
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(btnTrue2)
+                                    .addGroup(jPanel2Layout.createSequentialGroup()
+                                        .addComponent(cajero)
+                                        .addGap(27, 27, 27)
+                                        .addComponent(btnTrue1))))
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel3)
+                                    .addComponent(jLabel4))
+                                .addGap(16, 16, 16)
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtFieldClave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtFieldCedula, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(jPanel2Layout.createSequentialGroup()
+                                        .addComponent(txtFieldNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(lblExistencia))))))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(135, 135, 135)
+                        .addComponent(btnAgregar)))
+                .addContainerGap(138, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtFieldNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblExistencia))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtFieldCedula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(txtFieldClave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addComponent(jLabel6)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnAgregar)
+                        .addGap(27, 27, 27))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(35, 35, 35)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(vendedor)
+                            .addComponent(cajero)
+                            .addComponent(btnTrue1))
+                        .addGap(8, 8, 8)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btnTrue2)
+                            .addComponent(btnTrue3))
+                        .addContainerGap(94, Short.MAX_VALUE))))
+        );
+
+        jTabbedPane2.addTab("Inclusion de Empleado", jPanel2);
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 592, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 297, Short.MAX_VALUE)
+        );
+
+        jTabbedPane2.addTab("Modificacion de Empleado", jPanel3);
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 592, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 297, Short.MAX_VALUE)
+        );
+
+        jTabbedPane2.addTab("Borrar Empleado", jPanel4);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 592, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 297, Short.MAX_VALUE)
+        );
+
+        jTabbedPane2.addTab("Buscar Empleado", jPanel1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(Busqueda)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(Inclusion)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(Consulta)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(Modificacion)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(Borrado)
-                        .addGap(0, 4, Short.MAX_VALUE)))
-                .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(152, 152, 152)
                 .addComponent(jLabel1)
-                .addGap(78, 78, 78))
+                .addContainerGap(187, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jTabbedPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 597, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(28, 28, 28)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(26, 26, 26)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Busqueda)
-                    .addComponent(Inclusion)
-                    .addComponent(Modificacion)
-                    .addComponent(Consulta)
-                    .addComponent(Borrado))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(19, 19, 19))
+                .addContainerGap(345, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addContainerGap(57, Short.MAX_VALUE)
+                    .addComponent(jTabbedPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap()))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    private void setBlank() {
+        lblExistencia.setText("");
+        txtFieldCedula.setText("");
+        txtFieldNombre.setText("");
+        txtFieldClave.setText("");
+    }
+    private void txtFieldNombreFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtFieldNombreFocusLost
+        if (!ctrl.verificarExistenciaEmpleado(txtFieldCedula.getText())) {
+            lblExistencia.setForeground(Color.GREEN);
+            lblExistencia.setText("✔");
+        } else {
+            lblExistencia.setForeground(Color.RED);
+            lblExistencia.setText("✘");
+        }
+    }//GEN-LAST:event_txtFieldNombreFocusLost
 
-    private void InclusionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InclusionActionPerformed
+    private void btnAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarActionPerformed
+
+        String nombre = txtFieldNombre.getText();
+        String cedula = txtFieldCedula.getText();
+        String clave = txtFieldClave.getText();
+        boolean ven = (vendedor.isSelected()) ? true : false;
+        boolean caj = (cajero.isSelected()) ? true : false;
+        boolean desp = (btnTrue1.isSelected()) ? true : false;
+        boolean bodeg = (btnTrue3.isSelected()) ? true : false;
+        boolean adm = (btnTrue2.isSelected()) ? true : false;
+
+        if (nombre != "" && cedula != "" && clave != "") {
+            if (lblExistencia.getText() == "✔") {
+                ctrl.addEmpleado(cedula, nombre, clave, true, ven, caj, desp, bodeg, adm);
+                setBlank();
+            }
+        }
+    }//GEN-LAST:event_btnAgregarActionPerformed
+
+    private void btnTrue1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTrue1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_InclusionActionPerformed
+    }//GEN-LAST:event_btnTrue1ActionPerformed
 
-    private void ModificacionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ModificacionActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_ModificacionActionPerformed
-
-    private void ConsultaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ConsultaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_ConsultaActionPerformed
-
-    
     private Control ctrl;
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Borrado;
-    private javax.swing.JButton Busqueda;
-    private javax.swing.JButton Consulta;
-    private javax.swing.JButton Inclusion;
-    private javax.swing.JButton Modificacion;
+    private javax.swing.JButton btnAgregar;
+    private javax.swing.JRadioButton btnTrue1;
+    private javax.swing.JRadioButton btnTrue2;
+    private javax.swing.JRadioButton btnTrue3;
+    private javax.swing.JRadioButton cajero;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel panel;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JTabbedPane jTabbedPane2;
+    private javax.swing.JLabel lblExistencia;
+    private javax.swing.JTextField txtFieldCedula;
+    private javax.swing.JTextField txtFieldClave;
+    private javax.swing.JTextField txtFieldNombre;
+    private javax.swing.JRadioButton vendedor;
     // End of variables declaration//GEN-END:variables
 }
--- SistemaFerreteria/src/control/Control.java
+++ SistemaFerreteria/src/control/Control.java
@@ -17,7 +17,7 @@ import modelo.productos.Producto;
  * @author Michael Chen W.
  */
 public class Control {
-    
+
     public Control() {
         modelo = Modelo.getInstance();
         login = new VentanaLogin(this);
@@ -25,26 +25,28 @@ public class Control {
         ventas = new VentanaVentas(this);
         clientes = new VCatalogoClientes(this);
         productos = new VCatalogoProductos(this);
+        empleados = new VMantListaEmple(this);
+
         //
         setObservers();
     }
-    
+
     private void setObservers() {
         //this keeeps going down.
     }
-    
+
     public void mostrarLogin() {
         login.show();
     }
-    
+
     public void ocultarLogin() {
         login.dispose();
     }
-    
+
     public void mostrarMenu() {
         menu.show();
     }
-    
+
     public boolean login(String username, String password) {
         Empleado emp = modelo.verifyCredentials(username, password);
         if (emp != null) {
@@ -57,23 +59,23 @@ public class Control {
         }
         return false;
     }
-    
+
     public Cliente searchClienteByID(String id) {
         return modelo.searchClientByID(id);
     }
-    
+
     public LinkedList<Cliente> searchClienteByName(String name) {
         return modelo.searchClientByName(name);
     }
-    
+
     public Cliente createCLiente(String cedula, String nombre, String telefono, String email, int descuento) {
         return modelo.createCliente(cedula, nombre, telefono, email, descuento);
     }
-    
+
     public void addCliente(Cliente cliente) throws Exception {
         modelo.addCliente(cliente);
     }
-    
+
     public void deleteCliente(String id) throws Exception {
         modelo.deleteClient(id);
     }
@@ -81,23 +83,37 @@ public class Control {
     public void updateCliente(Cliente cliente) throws Exception {
         modelo.updateClient(cliente);
     }
-    
+
     public void mostrarClientes() {
         clientes.show();
     }
-    
+
     public void mostrarVentas() {
         ventas.show();
     }
-    
+
+    public void mostrarVEmpleados() {
+        empleados.show();
+    }
+
     public void mostrarProductos() {
         productos.show();
     }
+
     public void addProducto(String codigo, String descripcion, String medida, double precio, boolean activo) {
         modelo.addProducto(codigo, descripcion, medida, precio, activo);
     }
+
     public boolean verificarExistenciaProducto(String codigo) {
         return modelo.verificarExistenciaProducto(codigo);
+    }
+
+    public boolean verificarExistenciaEmpleado(String emp) {
+        return modelo.verificarExistenciaEmpleado(emp);
+    }
+
+    public void addEmpleado(String idEmpleado, String nombre, String clave, boolean activo, boolean vendedor, boolean cajero, boolean despachador, boolean bodeguero, boolean administrador) {
+        modelo.addEmpleado(idEmpleado, nombre, clave, activo, vendedor, cajero, despachador, bodeguero, administrador);
     }
 
     private Modelo modelo;
@@ -106,4 +122,5 @@ public class Control {
     private VentanaVentas ventas;
     private VCatalogoClientes clientes;
     private VCatalogoProductos productos;
+    private VMantListaEmple empleados;
 }
